de2f673344c9ceb8cb959564e75a903bd8e66bd7
RokasBarkevicius <rokasbarkevicius@gmail.com>
2024-01-02 14:46:13 +0200

    Update t.py

--- t.py
+++ t.py
@@ -2,20 +2,19 @@ import logging
 _logger = logging.getLogger(__name__)
 
 a = 5
-#   _logger.info(a)
+_logger.info(a)
 b = 6
 c = a*b +7
-#   _logger.debug(
-#     "%s\nhello\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s" %(
-#    	"C" * 100,
-#     "N" * 100,
-#     "C" * 100,
-#    	"C" * 100,
-#    	"B" * 100,
-#     "W" * 100,
-#    	"C" * 100,
-#    	"D" * 100,
-#     "H" * 100,
-#       )
-# )
+_logger.debug(
+    "%s\nhello\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s" %(
+    "C" * 100,
+    "N" * 100,
+    "C" * 100,
+    "C" * 100,
+    "B" * 100,
+    "W" * 100,
+    "C" * 100,
+    "D" * 100,
+    "H" * 100,
+    ))
 print(f"{c}")
