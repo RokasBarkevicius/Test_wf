769925d59fc034ddb515f54f6022ea7050200202
RokasBarkevicius <rokasbarkevicius@gmail.com>
2024-01-03 14:58:05 +0200

    Update __manifest__.py

--- two/__manifest__.py
+++ two/__manifest__.py
@@ -1,2 +1,19 @@
-v
-k=3333333
+# -*- coding: utf-8 -*-
+{
+    'name': "template",
+	"version": "15.0.0.0",
+    "summary": """ """,
+    "description": """ """,
+    "category": "Category",
+    "author": "Baltic Shipping",
+    "website": "Website",
+    "license": "Lo=icense",
+    'depends': ["depends"],
+    'data':[
+        'data/data.xml',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
