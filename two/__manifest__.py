011ecdbdb2405d487abb921cc418c8c34496eddb
RokasBarkevicius <rokasbarkevicius@gmail.com>
2024-01-10 16:42:29 +0200

    d

--- two/__manifest__.py
+++ two/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "template",
-    "version" "hhhh"
+    "version": "hhhh"
     "summar": """ edfvhdg""",
     "description": """ """,
     "category": "Category",
