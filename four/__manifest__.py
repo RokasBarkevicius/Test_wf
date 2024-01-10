93a534fd2661bf431bc148c8863453c80ccdc142
RokasBarkevicius <rokasbarkevicius@gmail.com>
2024-01-10 13:26:48 +0200

    ljk

---
+++ four/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "template",
+	'version': "",
+    "summar": """ edfvhdg""",
+    "description": """ """,
+    "category": "Category",
+    "author": "Baltic Shipping",
+    "website": "Website",
+    "license": "License",
+    'depends': ["depends"],
+    'data':[
+        'data/data.xml',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
