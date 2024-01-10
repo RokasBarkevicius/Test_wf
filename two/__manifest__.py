32151f6865c0ab648119c1e52b8d6ff4db10ff48
RokasBarkevicius <rokasbarkevicius@gmail.com>
2024-01-10 16:26:44 +0200

    f

--- two/__manifest__.py
+++ two/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "template",
-"this is me",
+    'version': "15.0.0.3"
     "summar": """ edfvhdg""",
     "description": """ """,
     "category": "Category",
